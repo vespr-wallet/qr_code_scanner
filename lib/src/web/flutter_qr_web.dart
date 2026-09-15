95ccd21a930e365541edf13c6c4decb861db8ece
Alexandru Dochioiu <alex@dochioiu.com>
2026-09-15 12:36:10 +0300

    chore: resolve existing Dart format and lint failures

--- lib/src/web/flutter_qr_web.dart
+++ lib/src/web/flutter_qr_web.dart
@@ -37,8 +37,9 @@ class WebQrView extends StatefulWidget {
   State<StatefulWidget> createState() => _WebQrViewState();
 
   static Future<bool> cameraAvailable() async {
-    final sources =
-        await web.window.navigator.mediaDevices.enumerateDevices().toDart;
+    final sources = await web.window.navigator.mediaDevices
+        .enumerateDevices()
+        .toDart;
     // List<String> vidIds = [];
     var hasCam = false;
     for (final e in sources.toDart) {
@@ -88,8 +89,9 @@ class _WebQrViewState extends State<WebQrView> {
   Future start() async {
     await _makeCall();
     _frameIntervall?.cancel();
-    _frameIntervall =
-        Timer.periodic(const Duration(milliseconds: 200), (timer) {
+    _frameIntervall = Timer.periodic(const Duration(milliseconds: 200), (
+      timer,
+    ) {
       _captureFrame2();
     });
   }
@@ -140,7 +142,8 @@ class _WebQrViewState extends State<WebQrView> {
               .toList();
 
           // attempt to find a main/primary camera. If none, use last back camera entry if it exists
-          final web.MediaDeviceInfo? idealCameraMediaInfo = backCameras
+          final web.MediaDeviceInfo? idealCameraMediaInfo =
+              backCameras
                   .where(
                     (camera) =>
                         camera.label.toLowerCase().contains('main') ||
@@ -152,25 +155,26 @@ class _WebQrViewState extends State<WebQrView> {
           constraints = UserMediaOptions(
             video: idealCameraMediaInfo != null
                 ? VideoOptions(
-                    deviceId:
-                        DeviceIdOptions(exact: idealCameraMediaInfo.deviceId),
+                    deviceId: DeviceIdOptions(
+                      exact: idealCameraMediaInfo.deviceId,
+                    ),
                   )
-                : VideoOptions(
-                    facingMode: "environment",
-                  ),
+                : VideoOptions(facingMode: "environment"),
           );
       }
 
       if (_controller == null) {
-        _controller = QRViewControllerWeb(this);
+        _controller = QRViewControllerWeb._(this);
         widget.onPlatformViewCreated(_controller!);
       }
       var stream = await getUserMedia(constraints).toDart;
       widget.onPermissionSet?.call(_controller!, true);
       _localStream = stream;
       video.srcObject = _localStream;
-      video.setAttribute('playsinline',
-          'true'); // required to tell iOS safari we don't want fullscreen
+      video.setAttribute(
+        'playsinline',
+        'true',
+      ); // required to tell iOS safari we don't want fullscreen
       await video.play().toDart;
     } catch (e) {
       cancel();
@@ -231,8 +235,9 @@ class _WebQrViewState extends State<WebQrView> {
       final code = jsQR(imgData.data, canvas.width, canvas.height);
       // ignore: unnecessary_null_comparison
       if (code != null && code.data != null) {
-        _scanUpdateController
-            .add(Barcode(code.data, BarcodeFormat.qrcode, code.data.codeUnits));
+        _scanUpdateController.add(
+          Barcode(code.data, BarcodeFormat.qrcode, code.data.codeUnits),
+        );
       }
     } on NoSuchMethodError {
       // Do nothing, this exception occurs continously in web release when no
@@ -293,7 +298,7 @@ class QRViewControllerWeb implements QRViewController {
   @override
   bool disposed = false;
 
-  QRViewControllerWeb(this._state);
+  QRViewControllerWeb._(this._state);
 
   @Deprecated(
     "Disposing the QRViewController is no longer necessary. The controller will self-dispose when the QRView is un-mounted.",
@@ -387,10 +392,12 @@ class QRViewControllerWeb implements QRViewController {
   }
 }
 
-Widget createWebQrView(
-        {onPlatformViewCreated, onPermissionSet, CameraFacing? cameraFacing}) =>
-    WebQrView(
-      onPlatformViewCreated: onPlatformViewCreated,
-      onPermissionSet: onPermissionSet,
-      cameraFacing: cameraFacing,
-    );
+Widget createWebQrView({
+  dynamic onPlatformViewCreated,
+  dynamic onPermissionSet,
+  CameraFacing? cameraFacing,
+}) => WebQrView(
+  onPlatformViewCreated: onPlatformViewCreated,
+  onPermissionSet: onPermissionSet,
+  cameraFacing: cameraFacing,
+);
